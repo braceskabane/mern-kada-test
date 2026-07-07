60c41ae848f66679754b74b5c5b7212619473303
braceskabane <allzoldyck31@gmail.com>
2026-07-07 16:54:44 +0700

    feat: add get me with protect token cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -79,4 +79,24 @@ const logout = (req, res) => {
   res.status(200).json({ message: "User logged out successfully" });
 };
 
-module.exports = { generateToken, setTokenCookie, register, login, logout };
+const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Get user failed:", error);
+    res.status(500).json({ message: "Get user failed" });
+  }
+};
+
+module.exports = {
+  generateToken,
+  setTokenCookie,
+  register,
+  login,
+  logout,
+  getMe,
+};
